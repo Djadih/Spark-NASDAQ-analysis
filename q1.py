e5952a765cb9491c6dcf21428c0357d6c0ce7b5c
Xiang-Gu <xiangu@utexas.edu>
2020-10-15 12:30:50 -0500

    finished q1

--- q1.py
+++ q1.py
@@ -1,37 +1,50 @@
 import pyspark
-
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType 
 from pyspark.sql.types import ArrayType, DoubleType, BooleanType
 from pyspark.sql.functions import col,array_contains
-
 import os
 
 def read_csv(file_name):
-    df = spark.read.option("header",True).csv(file_name)
-    return df
-
-def write_csv(df):
-    df.write.option("header",True) \
-        .csv("/home/hussam/Documents/spark/tmp.csv")
+    return spark.read.option("header",True).csv(file_name)
 
 def custom_filter(df):
-    filtered_df = df.filter((df.Close - df.Open) / (df.Open) >= 0.01)
-    filtered_df.show(truncate=False)
-    return filtered_df
+    return df.filter((df.Close - df.Open) / (df.Open) >= 0.01)
 
-spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+def remove_csv_suffix(fileName):
+	return fileName[:-4]
 
-# os.chdir("/home/hussam/Documents/spark/NASDAQ100/")
+def write_output(result, outputFileName):
+	# assume result contains (val1, val2) tuples
+	with open(outputFileName, "w") as f:
+		for val1, val2 in result:
+			f.write(val1 + "," + val2 + "\n")
 
-d = "/home/hussam/Documents/spark/NASDAQ100/"
-for fileName in os.listdir(d):
-    fileName = os.path.join(d, fileName)
 
-    print(fileName)
-    df = read_csv(fileName)
-    df.printSchema()
-    specific_df = df.select("Open", "Close")
-    specific_df.printSchema()
-    filtered_df = custom_filter(df)
-    print("There are", filtered_df.count(),"rows.")
+if __name__ == "__main__":
+	spark = SparkSession.builder.getOrCreate()
+
+	result = [] # a list of (companyName, #OfProfitableDays) tuples
+
+	path = os.getcwd() + "/NASDAQ100/"
+	for fileName in os.listdir(path):
+		# assuming all files in "path" are .csv files
+		companyName = remove_csv_suffix(fileName)
+
+	    # Read each .csv file
+		df = read_csv(path + fileName)
+
+	    # Select only "Open" and "Close" column
+		df = df.select("Open", "Close")
+
+	    # Filter rows to retains only those rows that satisfy condition "(close - open) / open >= 0.01"
+		df = custom_filter(df)
+
+		# Output each file as one line "name,days"
+		result.append((str(companyName), str(df.count())))
+
+	# Sort output according to company name
+	result.sort()
+
+	# Write out result to "output_1.txt"
+	write_output(result, "output_1.txt")
